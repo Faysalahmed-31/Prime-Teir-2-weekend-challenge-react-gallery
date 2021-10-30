8cce6fc5701bb06cc24f27acb21a917343608352
Faisal Abdulle <fabdulle@Faisals-MacBook-Air.local>
2021-10-30 09:33:39 -0500

    added axios.get to get data from the server to the client

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const[galleryItems, setgalleryItems] = useState( [] );
+
+  useEffect( ()=>{
+    console.log( 'component loaded' );
+    getItems();
+  }, []);
+
+  const getItems=()=>{
+    axios.get( '/gallery' ).then( (response)=>{
+      console.log(response.data);
+      setgalleryItems( response.data );
+    }).catch((err)=>{
+      alert('nope');
+      console.log(err);
+    })
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
